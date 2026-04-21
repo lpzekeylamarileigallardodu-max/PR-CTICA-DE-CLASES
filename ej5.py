db782d7da681b07eb4ca1cca980514cdae985e72
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-04-21 18:31:44 -0400

    Create ej5.py
    class raíz cúbica

---
+++ ej5.py
@@ -0,0 +1,9 @@
+class raiz {
+    public double num { get; set; }
+
+    public double raiz() {
+        double xd = 1.0 / 3.0;
+        double uwu = Math.Pow(num, xd);
+        return uwu;
+    }
+}

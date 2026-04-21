c21d11a751b3be3b95ff39b1f683e8d5fc744441
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-04-21 18:29:31 -0400

    Create ej2.py
    cilindro

---
+++ ej2.py
@@ -0,0 +1,10 @@
+class xd {
+    public double radio { get; set; }
+    public double altura { get; set; }
+
+    public double sacarVolumen() {
+        double xd = Math.PI * Math.Pow(radio, 2) * altura;
+        double uwu = xd;
+        return uwu;
+    }
+}

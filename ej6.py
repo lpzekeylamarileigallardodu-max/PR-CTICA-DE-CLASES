ea7c1ab2fb1abe7bfb1b4af73b7369a9a7413335
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-04-21 18:33:02 -0400

    Create ej6.py
    clase de ecación

---
+++ ej6.py
@@ -0,0 +1,9 @@
+class qwerty {
+    public double valorX { get; set; }
+
+    public double resolver() {
+        double xd = (4 * Math.Pow(valorX, 3)) + (2 * Math.Pow(valorX, 2)) + (6 * valorX) - 5;
+        double uwu = xd;
+        return uwu;
+    }
+}

42f4216f00f60d3f2842b1bafec3230a98e83b63
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-04-28 12:16:34 -0400

    Update ej3.py

--- ej3.py
+++ ej3.py
@@ -1,12 +1,18 @@
-class distancia {
-    public double x1 { get; set; }
-    public double y1 { get; set; }
-    public double x2 { get; set; }
-    public double y2 { get; set; }
-
-    public double obtenerDistancia() {
-        double xd = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
-        double uwu = Math.Sqrt(xd);
-        return uwu;
-    }
-}
+import math
+class murodeberlinxd:
+    def __init__(self):
+        self.pim = 0
+        self.charlie = 0
+        self.jefe = 0
+        self.alan = 0
+    def metodo(self):
+        dis = ((self.charlie-self.pim)**2 + (self.alan-self.jefe)**2) ** 0.5
+        return dis    
+print("metele punto 1")
+objeto = murodeberlinxd()
+objeto.pim = float(input("distancia inicial a: "))
+objeto.charlie = float(input("distancia final a : "))
+objeto.jefe = float(input("distancia inicial b: "))
+objeto.alan = float(input("distancia final b: "))
+añia = objeto.metodo()
+print("la distancia que los separa es: ",añia)

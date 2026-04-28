2647737edc36cf59f581cfeb3258fca49c233c63
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-04-28 12:17:38 -0400

    Update ej6.py

--- ej6.py
+++ ej6.py
@@ -1,9 +1,13 @@
-class qwerty {
-    public double valorX { get; set; }
-
-    public double resolver() {
-        double xd = (4 * Math.Pow(valorX, 3)) + (2 * Math.Pow(valorX, 2)) + (6 * valorX) - 5;
-        double uwu = xd;
-        return uwu;
-    }
-}
+import math
+class exuacióndetercergrado:
+    def __init__(self):
+        self.X = 0
+        self.nosoyysoyw =0
+    def metodo(self):
+        nosoyysoyw = 4*(self.x**3) + 2*(self.x**2) + 6*self.x - 5
+        return nosoyysoyw    
+print("reealizar esta ecuación con x: Y = 4X³ + 2X² + 6X - 5")
+objeto = exuacióndetercergrado()
+objeto.x = float(input("ingrésa x: "))
+nosoyysoyw = objeto.metodo()
+print("el valor de y es: ",nosoyysoyw)

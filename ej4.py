4d859c6af3dda51a03eb1510f81996f9b9f3bc7d
nugget <lpze.keylamarilei.gallardo.du@unifranz.edu.bo>
2026-04-28 12:17:04 -0400

    Update ej4.py

--- ej4.py
+++ ej4.py
@@ -1,10 +1,19 @@
-class Auto {
-    public double costoBase { get; set; }
-
-    public double costoTotal() {
-        double xd = costoBase * 1.12; 
-        double uwu = xd * 1.06;   
-
-        return uwu;
-    }
-}
+import math
+class tomate:
+    def __init__(self):
+        self.precio = 0
+        self.gg = 0
+        self.ono = 0
+        self.tot = 0
+    def metodo(self):
+        gg = self.precio*0.12
+        ono = (gg+self.precio)*0.06
+        tot = self.precio+self.gg+self.ono
+        return gg,ono,tot    
+print("metele punto 1")
+objeto = tomate()
+objeto.precio = float(input("cuánto cuesta la cybertruck: "))
+gg,ono,tot = objeto.metodo()
+print("vas a ganar: ",gg)
+print("tus impuestos van a ser: ",ono,"(evádelos)")
+print("total ",tot)
